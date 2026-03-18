f350cfa5d4df450f57277885b4537b4609227bc7
franzap <_@franzap.com>
2026-03-18 15:54:11 -0300

    Config version

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import { readFileSync } from 'fs';
+
+const { version } = JSON.parse(readFileSync('./package.json', 'utf-8'));
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	define: {
+		'import.meta.env.APP_VERSION': JSON.stringify(`web/${version}`)
+	},
 
 	// Optimize dependencies
 	optimizeDeps: {
