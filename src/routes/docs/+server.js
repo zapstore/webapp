3b322a78675ca62454844db5a6cd55b3ee2df295
Niel Liesmons <130457889+NielLiesmons@users.noreply.github.com>
2026-06-05 14:18:32 +0200

    Prerender fix for docs

--- src/routes/docs/+server.js
+++
@@ -1,9 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export const prerender = true;
-
-/** @type {import('./$types').RequestHandler} */
-export function GET() {
-	throw redirect(308, '/docs/quickstart');
-}
-
